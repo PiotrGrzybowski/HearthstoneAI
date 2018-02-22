d1550cdbc0c5cf3c06d364c38db027afbe0fd349
Piotr <p.grzybowski2@gmail.com>
2018-02-22 16:58:57 +0100

    Initial commit with project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Heartstone AI',
+   version='1.0',
+   description='A useful module',
+   author='Piotr Grzybowski, Ewa Juralewicz',
+   author_email='grzybowski.piotr@outlook.com, eva.juralewicz@gmail.com',
+   packages=['HeartstoneAI'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
