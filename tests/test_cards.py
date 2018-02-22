cd3ddb99bf388d0886950d478a6d9988cde0c866
Piotr <p.grzybowski2@gmail.com>
2018-02-22 22:07:09 +0100

    Tests for atacking minion by minion

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -5,30 +5,63 @@ from HeartstoneAI.cards import CombatCard, Spell, Hero
 from HeartstoneAI.flow import deals_manage_to_opposite_player
 from HeartstoneAI.state import Player, State
 
-ABILITY_1 = partial(deals_manage_to_opposite_player, damage=2)
-
-CARD_1 = CombatCard(name='Ewcia', cost=1, abilities=[], attack=1, health=2)
-CARD_2 = CombatCard(name='Misio', cost=1, abilities=[], attack=1, health=2)
-CARD_3 = CombatCard(name='Pamewcia', cost=1, abilities=[], attack=1, health=2)
-CARD_4 = CombatCard(name='Pamisio', cost=1, abilities=[], attack=1, health=2)
-SPELL_1 = Spell(name='PaSpell', cost=1, abilities=[ABILITY_1])
-
-HERO_1 = Hero(name='Pamisio', cost=0, abilities=[], attack=0, health=20, hero_class=None)
-HERO_2 = Hero(name='Pamewcia', cost=0, abilities=[], attack=0, health=20, hero_class=None)
-
 
 class TestCards(unittest.TestCase):
+    def setUp(self):
+        self.ability_1 = partial(deals_manage_to_opposite_player, damage=2)
+
+        self.card_1 = CombatCard(name='C1', cost=1, abilities=[], attack=1, health=1)
+        self.card_2 = CombatCard(name='C2', cost=1, abilities=[], attack=1, health=1)
+
+        self.card_3 = CombatCard(name='C3', cost=1, abilities=[], attack=2, health=2)
+        self.card_4 = CombatCard(name='C4', cost=1, abilities=[], attack=2, health=2)
+
+        self.card_5 = CombatCard(name='C5', cost=1, abilities=[], attack=3, health=3)
+        self.card_6 = CombatCard(name='C6', cost=1, abilities=[], attack=3, health=3)
+
+        self.card_7 = CombatCard(name='C5', cost=1, abilities=[], attack=4, health=4)
+        self.card_8 = CombatCard(name='C6', cost=1, abilities=[], attack=4, health=4)
+
+        self.spell_1 = Spell(name='PaSpell', cost=1, abilities=[self.ability_1])
+
+        self.hero_1 = Hero(name='Pamisio', cost=0, abilities=[], attack=0, health=20, hero_class=None)
+        self.hero_2 = Hero(name='Pamewcia', cost=0, abilities=[], attack=0, health=20, hero_class=None)
+
+        self.first_player = Player(self.hero_1, [], [], [], [])
+        self.second_player = Player(self.hero_2, [], [], [], [])
+
+        self.state = State(self.first_player, self.second_player)
+
     def test_play_spell(self):
-        first_player = Player(hero=HERO_1, hand=[CARD_1, SPELL_1], deck=[CARD_2], board=[CARD_3], graveyard=[])
-        second_player = Player(hero=HERO_2, hand=[], deck=[], board=[], graveyard=[])
-        state = State(first_player, second_player)
+        self.first_player.hand = [self.card_1, self.spell_1]
+        self.first_player.board = [self.card_3]
 
-        state.play_card(1)
+        self.state.play_card(1)
 
-        self.assertEqual(first_player.hand, [CARD_1])
-        self.assertEqual(first_player.board, [CARD_3])
-        self.assertEqual(first_player.graveyard, [SPELL_1])
-        self.assertEqual(second_player.hero.health, 18)
+        self.assertEqual(self.first_player.hand, [self.card_1])
+        self.assertEqual(self.first_player.board, [self.card_3])
+        self.assertEqual(self.first_player.graveyard, [self.spell_1])
+        self.assertEqual(self.second_player.hero.health, 18)
+
+    def test_attack_minion_by_minion_1(self):
+        self.first_player.board = [self.card_1, self.card_3]
+        self.second_player.board = [self.card_2, self.card_4]
+
+        self.state.attack(0, 1)
+        self.assertEqual(self.first_player.graveyard, [self.card_1])
+        self.assertEqual(self.first_player.board, [self.card_3])
+        self.assertEqual(self.second_player.board, [self.card_2, self.card_4])
+        self.assertEqual(self.second_player.board[1].health, 1)
+
+    def test_attack_minion_by_minion_2(self):
+        self.first_player.board = [self.card_1, self.card_3]
+        self.second_player.board = [self.card_2, self.card_4]
+
+        self.state.attack(1, 0)
+        self.assertEqual(self.first_player.board, [self.card_1, self.card_3])
+        self.assertEqual(self.second_player.board, [self.card_4])
+        self.assertEqual(self.second_player.graveyard, [self.card_2])
+        self.assertEqual(self.first_player.board[1].health, 1)
 
 
 if __name__ == '__main__':
