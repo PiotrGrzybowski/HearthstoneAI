52237b9ec26bce3693bedaa8dac14da7f4309b24
Piotr Grzybowski <p.grzybowski2@gmail.com>
2018-03-04 21:20:19 +0100

    Generation of actions

--- HeartstoneAI/actions_generation.py
+++ HeartstoneAI/actions_generation.py
@@ -1,10 +1,13 @@
+import numpy as np
+
+
 class Card:
-    def __init__(self, name, value):
+    def __init__(self, name, cost):
         self.name = name
-        self.value = value
+        self.cost = cost
 
 
-def subset_sum_cards(cards, target, partial=None, partial_sum=0):
+def get_dubdrtd(cards, target, partial=None, partial_sum=0):
     if partial is None:
         partial = []
     if partial_sum <= target:
@@ -13,11 +16,30 @@ def subset_sum_cards(cards, target, partial=None, partial_sum=0):
         return
     for i, card in enumerate(cards):
         remaining = cards[i + 1:]
-        yield from subset_sum_cards(remaining, target, partial + [card.name], partial_sum + card.value)
+        yield from get_dubdrtd(remaining, target, partial + [card.name], partial_sum + card.cost)
+
+
+def get_cards_play_combinations(cards, indexes, target, partial=None, partial_sum=0):
+    if partial is None:
+        partial = []
+    if partial_sum <= target:
+        yield partial
+    if partial_sum >= target:
+        return
+    i = 0
+
+    for ind, c in zip(indexes, cards):
+        remaining = cards[i + 1:]
+        remaining_indexes = indexes[i + 1:]
+        i += 1
+        yield from get_cards_play_combinations(remaining, remaining_indexes, target, partial + [ind], partial_sum + c.cost)
 
 
 if __name__ == "__main__":
-    cards = [Card('1a', 1), Card('1b', 1), Card('2', 2), Card('3', 3), Card('6', 6)]
+    cards = [Card('6', 6), Card('1a', 1), Card('1b', 1), Card('2', 2), Card('3', 3)]
+    ind = np.arange(len(cards))
 
-    for c in subset_sum_cards(cards, 6):
-        print(c)
+    for c in get_cards_play_combinations(cards, ind, 6):
+        print("Score = {}".format(c))
+
+    # print(list(get_possible_cards_play(cards, 1)))
---
+++ tests/test_actions.py
@@ -0,0 +1,47 @@
+import numpy as np
+import unittest
+import json
+
+from HeartstoneAI.actions_generation import get_cards_play_combinations
+from HeartstoneAI.cards import Hero
+from HeartstoneAI.cards_generator import card_from_json
+from HeartstoneAI.state import Player, State
+
+
+class TestActions(unittest.TestCase):
+    def setUp(self):
+        with open('../HeartstoneAI/cards.json') as json_file:
+            data = json.load(json_file)
+            self.abusive_sergeant = card_from_json(data[0])
+            self.agent_squire = card_from_json(data[1])
+            self.divine_strength = card_from_json(data[2])
+            self.selfless_hero = card_from_json(data[3])
+            self.divine_favor = card_from_json(data[4])
+            self.seal_of_champions = card_from_json(data[5])
+            self.steward_of_darshire = card_from_json(data[6])
+            self.wolfrider = card_from_json(data[7])
+            self.blessing_of_kings = card_from_json(data[8])
+            self.defender_of_argus = card_from_json(data[9])
+
+        self.hero_1 = Hero(name='Pamisio', cost=0, abilities=dict(), attack=0, health=20, hero_class=None)
+        self.hero_2 = Hero(name='Pamewcia', cost=0, abilities=dict(), attack=0, health=20, hero_class=None)
+
+        self.first_player = Player(self.hero_1, [], [], [], [])
+        self.second_player = Player(self.hero_2, [], [], [], [])
+
+        self.state = State(self.first_player, self.second_player)
+
+    def test_how_many_combinations_of_playing_cards(self):
+        self.state.current_player.hand = [self.abusive_sergeant, self.agent_squire, self.divine_strength,
+                                          self.divine_favor]
+
+        player_hand = self.state.current_player.hand
+        indexes = np.arange(len(player_hand))
+
+        self.assertEqual(len(list(get_cards_play_combinations(player_hand, indexes, 0))), 1)
+        self.assertEqual(len(list(get_cards_play_combinations(player_hand, indexes, 1))), 4)
+        self.assertEqual(len(list(get_cards_play_combinations(player_hand, indexes, 2))), 7)
+        self.assertEqual(len(list(get_cards_play_combinations(player_hand, indexes, 3))), 9)
+
+    def test_cards_to_play_with_combination(self):
+        combination = [0, 1]
