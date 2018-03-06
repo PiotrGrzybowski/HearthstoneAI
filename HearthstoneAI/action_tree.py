b4b8437ba10ea6fb748822166eef4a98238b558d
evaj <eva.juralewicz@gmail.com>
2018-03-06 01:15:33 +0100

    tree generation fix

--- HearthstoneAI/action_tree.py
+++ HearthstoneAI/action_tree.py
@@ -16,6 +16,7 @@ def get_new_state(state, available_mana, evaluation_function):
     path = ''
     for elem in leafs:
         evaluation = evaluation_function(elem[0])
+        # print(elem[1])
         if evaluation > max:
             best_state = elem[0]
             path = elem[1]
@@ -40,18 +41,20 @@ def walk(state, current_mana, available_mana, leafs, lol='', path=''):
 def walk_attacks(state, leafs, lol='', path=''):
     for index, card in enumerate(state.current_player.board):
         new_state = deepcopy(state)
+        current_card = new_state.current_player.board[index]
         if isinstance(card, Minion) and not card.summoning_sickness:
+            if current_card in new_state.current_player.board and not current_card.summoning_sickness:
+                path += 'Attacking HERO with ' + card.name + ' -> '
+                print(lol + 'Attacking HERO with ' + card.name)
+                new_state.attack_hero_by_ref(current_card)
+                walk_attacks(new_state, leafs, lol + '\t', path)
             for opponent_card in new_state.opposite_player.board:
-                if isinstance(opponent_card, Minion):
+                if isinstance(opponent_card, Minion) and not current_card.summoning_sickness:
                     path += 'battling ' + card.name + ' and ' + opponent_card.name + ' -> '
                     print(lol + 'battling ' + card.name + ' and ' + opponent_card.name)
                     new_state.attack_by_ref(new_state.current_player.board[index], opponent_card)
                     walk_attacks(new_state, leafs, lol + '\t', path)
-            newer_state = deepcopy(state)
-            path += 'Attacking HERO with ' + card.name + ' -> '
-            print(lol + 'Attacking HERO with ' + card.name)
-            newer_state.attack_hero(index)
-            walk_attacks(newer_state, leafs, lol + '\t', path)
+            # newer_state = deepcopy(state)
     path += 'END'
     print(lol + 'END')
     leafs.append((state, path))
